52908550e780fd98c82359dffbcda7b36acc304c
lloydbenson <lloyd.benson@gmail.com>
2014-09-13 04:47:39 -0500

    add template

---
+++ server.js
@@ -0,0 +1,61 @@
+var Hapi = require('hapi');
+var Purdy = require('purdy');
+var Gills = require('./index');
+
+var server = new Hapi.Server(8080);
+var options = {};
+
+server.pack.register({ plugin: Gills, options: options }, function(err) {
+   if (err) {
+       console.log('plugin did not load');
+   }
+});
+
+server.on('log', function (event) {
+
+    console.log(JSON.stringify(event));
+});
+
+server.on('internalError', function (request, error) {
+
+    console.error(JSON.stringify(error));
+});
+
+server.on('request', function (request, event, tags) {
+
+    if (!tags.response) {
+        return;
+    }
+
+    var reqToRender = {
+        time: new Date(),
+        requestId: request.id,
+        headers: request.headers,
+        method: request.method,
+        path: request.path,
+        query: request.query,
+        payload: request.payload,
+        tags: tags ? Object.keys(tags) : ''
+    };
+
+    console.log('------ REQUEST RECEIVED -------');
+    Purdy(reqToRender);
+    console.log('-------------------------------');
+});
+
+server.on('response', function (request) {
+
+    var reqToRender = {
+        headers: request.response.headers,
+        payload: request.response.source
+    };
+
+    console.log('------- RESPONSE SENT ---------');
+    Purdy(reqToRender);
+    console.log('-------------------------------');
+});
+
+server.start(function () {
+
+    console.log('started server: ' + server.info.uri);
+});
