5371d272089439c09e340239c7689a5b93971077
lloydbenson <lloyd.benson@gmail.com>
2014-09-28 15:57:21 -0500

    added cancel functionality

--- test/gills.js
+++ test/gills.js
@@ -170,6 +170,57 @@ describe('', function () {
         });
     });
 
+    it('gills cancelled run', function (done) {
+        internals.prepareServer(function (server) {
+
+            var payload = {
+                name: 'cancel',
+                description: 'description',
+                scm_type: 'git',
+                scm_url: 'https://github.com/fishin/pail',
+                scm_branch: 'master',
+                head: 'npm install',
+                body: 'npm test',
+                tail: ''
+            };
+            server.inject({ method: 'POST', url: '/gills/job', payload: payload}, function (response) {
+
+                expect(response.statusCode).to.equal(302);
+                var job_id = server.plugins.tacklebox.getJobs()[0];
+                expect(job_id).to.exist;
+                server.inject({ method: 'GET', url: '/gills/job/'+job_id+ '/start'}, function (response) {
+                    
+                    console.log('starting job: ' + job_id);
+                    //expect(response.statusCode).to.equal(302);
+                    var run_id = server.plugins.tacklebox.getRuns(job_id)[0].id;
+                    expect(run_id).to.exist; 
+                    server.inject({ method: 'GET', url: '/gills/job/'+job_id+ '/run/' + run_id + '/cancel'}, function (response) {
+                        expect(response.statusCode).to.equal(302);
+                    
+                        var intervalObj = setInterval(function() {
+
+                            var run = server.plugins.tacklebox.getRun(job_id, run_id);
+                            if (run.finishTime) {
+                                clearInterval(intervalObj);
+                                expect(run.finishTime).to.exist;
+                                expect(run.status).to.equal('cancelled');
+                                server.inject({ method: 'GET', url: '/gills/job/'+job_id}, function (response) {
+                        
+                                    expect(response.statusCode).to.equal(200);
+                                    server.inject({ method: 'GET', url: '/gills/job/'+job_id+ '/delete'}, function (response) {
+
+                                        expect(response.statusCode).to.equal(302);
+                                        done();
+                                    });
+                                });
+                            }
+                        }, 1000);
+                    });
+                });
+            });
+        });
+    });
+
     it('gills reel crud flow', function (done) {
         internals.prepareServer(function (server) {
 
