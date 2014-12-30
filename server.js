61f3e6413e47b5cea44df0e4803592c19d83ea49
lloydbenson <lloyd.benson@gmail.com>
2014-12-30 10:35:36 -0600

    minor fixes

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ server.register({ register: Lout, options: options }, function(err) {
    }
 });
 
-//server.pack.register({ register: Tacklebox , options: options }, function(err) {
-//   if (err) {
-//       console.log('tacklebox did not load');
-//   }
-//});
-
 server.on('log', function (event) {
 
     console.log(JSON.stringify(event));
@@ -34,42 +28,6 @@ server.on('internalError', function (request, error) {
 
     console.error(JSON.stringify(error));
 });
-/*
-server.on('request', function (request, event, tags) {
-
-    if (!tags.response) {
-        return;
-    }
-
-    var reqToRender = {
-        time: new Date(),
-        requestId: request.id,
-        headers: request.headers,
-        method: request.method,
-        path: request.path,
-        query: request.query,
-        payload: request.payload,
-        tags: tags ? Object.keys(tags) : ''
-    };
-
-    console.log('------ REQUEST RECEIVED -------');
-    console.log(reqToRender);
-    console.log('-------------------------------');
-});
-
-server.on('response', function (request) {
-
-    var reqToRender = {
-        headers: request.response.headers,
-        payload: request.response.source
-    };
-
-    console.log('------- RESPONSE SENT ---------');
-    console.log(reqToRender);
-    console.log('-------------------------------');
-});
-
-*/
 
 server.start(function () {
 
