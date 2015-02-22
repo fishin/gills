eb9453686b383d8febbc3ac572336aba04c333f3
lloydbenson <lloyd.benson@gmail.com>
2015-02-21 19:25:45 -0600

    add merge code magic and some minor fixes

--- views/helpers/userTypeRadio.js
+++ views/helpers/userTypeRadio.js
@@ -1,12 +1,15 @@
 var Handlebars = require('handlebars');
 
-module.exports = function (type) {
+module.exports = function (context) {
 
     var output = '';
     var types = [ 'local', 'github' ];
+    if (context.name === 'admin') {
+        types = [ 'local' ];
+    }
     for (var i = 0; i < types.length; i++) {
         output += '<div class="btn-group"><span class="input-group-addon"><input type="radio" name="type" value="' + types[i] + '"';
-        if (types[i] === type ) {
+        if (types[i] === context.type ) {
             output += ' checked';
         }
         output +='> </span> <button type="button" class="btn btn-default">' + types[i] + '</button></div>';
