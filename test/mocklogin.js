890a8a55d537245378518d263adaf39e34302946
lloydbenson <lloyd.benson@gmail.com>
2015-04-26 16:30:43 -0500

    more mocking and user fix

--- test/mocklogin.js
+++ test/mocklogin.js
@@ -28,7 +28,6 @@ internals.prepareServer = function (callback) {
     callback(server);
 };
 
-
 describe('mock login', function () {
 
     it('GET /view/login', function (done) {
@@ -55,7 +54,7 @@ describe('mock login', function () {
         });
     });
 
-    it('POST /view/job', function (done) {
+    it('POST /view/login lloyd', function (done) {
 
         var type = 'tacklebox';
         var routes = [
@@ -70,7 +69,7 @@ describe('mock login', function () {
                 file: 'index.json'
             },
             {
-                method: 'get',
+                method: 'post',
                 path: '/api/user/12345678-1234-1234-1234-123456789012/validate',
                 file: 'true'
             }
@@ -97,6 +96,146 @@ describe('mock login', function () {
                         expect(artifacts.name).to.equal('lloyd');
                         expect(artifacts.displayName).to.equal('Lloyd Benson');
                         expect(artifacts.type).to.equal('local');
+                        server.inject({ method: 'GET', url: '/view/logout' }, function (response) {
+
+                            expect(response.statusCode).to.equal(302);
+                            expect(response.request.auth.artifacts).to.not.exist();
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('POST /view/login admin', function (done) {
+
+        var type = 'tacklebox';
+        var routes = [
+            {
+                method: 'get',
+                path: '/api/user/byname/admin',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/api/user/12345678-1234-1234-1234-123456789013',
+                file: 'index.json'
+            },
+            {
+                method: 'post',
+                path: '/api/user/12345678-1234-1234-1234-123456789013/validate',
+                file: 'true'
+            }
+        ];
+        Mock.prepareServer(type, routes, function (mockServer) {
+
+            mockServer.start(function () {
+
+                internals.defaults.api = {
+                    url: mockServer.info.uri + '/api'
+                };
+                var payload = {
+                    name: 'admin',
+                    type: 'local',
+                    password: 'password'
+                };
+                internals.prepareServer(function (server) {
+
+                    server.inject({ method: 'POST', url: '/view/login', payload: payload }, function (response) {
+
+                        expect(response.statusCode).to.equal(302);
+                        var artifacts = response.request.auth.artifacts;
+                        expect(artifacts.userId.length).to.equal(36);
+                        expect(artifacts.name).to.equal('admin');
+                        expect(artifacts.displayName).to.equal('Admin');
+                        expect(artifacts.type).to.equal('local');
+                        server.inject({ method: 'GET', url: '/view/logout' }, function (response) {
+
+                            expect(response.statusCode).to.equal(302);
+                            expect(response.request.auth.artifacts).to.not.exist();
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('POST /view/login badpass', function (done) {
+
+        var type = 'tacklebox';
+        var routes = [
+            {
+                method: 'get',
+                path: '/api/user/byname/lloyd',
+                file: 'index.json'
+            },
+            {
+                method: 'get',
+                path: '/api/user/12345678-1234-1234-1234-123456789012',
+                file: 'index.json'
+            },
+            {
+                method: 'post',
+                path: '/api/user/12345678-1234-1234-1234-123456789012/validate',
+                file: 'false'
+            }
+        ];
+        Mock.prepareServer(type, routes, function (mockServer) {
+
+            mockServer.start(function () {
+
+                internals.defaults.api = {
+                    url: mockServer.info.uri + '/api'
+                };
+                var payload = {
+                    name: 'admin',
+                    type: 'local',
+                    password: 'password'
+                };
+                internals.prepareServer(function (server) {
+
+                    server.inject({ method: 'POST', url: '/view/login', payload: payload }, function (response) {
+
+                        expect(response.statusCode).to.equal(302);
+                        expect(response.request.auth.artifacts).to.not.exist();
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    it('POST /view/login invalid', function (done) {
+
+        var type = 'tacklebox';
+        var routes = [
+            {
+                method: 'get',
+                path: '/api/user/byname/invalid',
+                file: 'index.json'
+            }
+        ];
+        Mock.prepareServer(type, routes, function (mockServer) {
+
+            mockServer.start(function () {
+
+                internals.defaults.api = {
+                    url: mockServer.info.uri + '/api'
+                };
+                var payload = {
+                    name: 'invalid',
+                    type: 'local',
+                    password: 'password'
+                };
+                internals.prepareServer(function (server) {
+
+                    server.inject({ method: 'POST', url: '/view/login', payload: payload }, function (response) {
+
+
+                        expect(response.statusCode).to.equal(302);
+                        expect(response.request.auth.artifacts).to.not.exist();
                         done();
                     });
                 });
