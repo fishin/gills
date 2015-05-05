71dbefd0e9cadc6660cf6024aa253e86e3418296
lloydbenson <lloyd.benson@gmail.com>
2015-05-05 11:09:09 -0500

    add email functionality

---
+++ views/helpers/notifyTypeRadio.js
@@ -0,0 +1,15 @@
+var Handlebars = require('handlebars');
+
+module.exports = function (context) {
+
+    var output = '';
+    var types = [ 'none', 'email' ];
+    for (var i = 0; i < types.length; i++) {
+        output += '<div class="btn-group"><span class="input-group-addon"><input type="radio" name="notifyType" value="' + types[i] + '"';
+        if (types[i] === context.notifyType ) {
+            output += ' checked';
+        }
+        output += '> </span> <button type="button" class="btn btn-default">' + types[i] + '</button></div>';
+    }
+    return new Handlebars.SafeString(output);
+};
